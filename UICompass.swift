0cf6006c1b91e47497ebf6c3568292148939c77c
Jackson Utsch <jutechs@gmail.com>
2017-03-28 22:19:52 -0700

    Add files via upload

--- UICompass.swift
+++ UICompass.swift
@@ -5,7 +5,6 @@
 //  Created by Jackson Utsch on 9/5/16.
 //  Copyright © 2016 Jackson Utsch. All rights reserved.
 //
-
 import UIKit
 import CoreLocation
 
@@ -13,15 +12,13 @@ import CoreLocation
 /// - Author: Jackson Utsch
 /// - NOTE: Generally keep the width and height equal
 public class UICompass:UIView, CLLocationManagerDelegate {
-    // Customizable variables
-    /// - NOTE: Default: UIColor.whiteColor()
-    var heading:enumNorth = .trueNorth // Determines if user is using true or magnetic north
-    /// - NOTE: Default: UIColor.whiteColor()
-    var innerRingColor:UIColor = UIColor.whiteColor()
-
+    
+    private let heading:enumNorth = .trueNorth
+    private let innerRingColor:UIColor = UIColor.white
+    private let pointerDiameter:Double = 40 // Set desired pointer diameter
+    
     private var LM:CLLocationManager!
     private var northTick:UIView!
-    private let pointerDiameter:Double = 40 // Set desired pointer diameter
     required public init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
     }
@@ -30,11 +27,11 @@ public class UICompass:UIView, CLLocationManagerDelegate {
         layer.cornerRadius = min(frame.width/2, frame.height/2)
         clipsToBounds = true
         layer.borderWidth = 3
-        layer.borderColor = UIColor.grayColor().CGColor
+        layer.borderColor = UIColor.gray.cgColor
         
         northTick = UIView(frame: CGRect(x: 0, y: 0, width: pointerDiameter, height: pointerDiameter))
         northTick.clipsToBounds = true
-        northTick.backgroundColor = UIColor.blackColor()
+        northTick.backgroundColor = UIColor.black
         northTick.layer.cornerRadius = min(northTick.frame.width/2, northTick.frame.height/2)
         northTick.center.x = 0
         northTick.center.y = 0
@@ -46,31 +43,28 @@ public class UICompass:UIView, CLLocationManagerDelegate {
         innerRing.clipsToBounds = true
         innerRing.layer.cornerRadius = min(innerRing.frame.width/2, innerRing.frame.height/2)
         innerRing.layer.borderWidth = 3
-        innerRing.layer.borderColor = UIColor.blackColor().CGColor
+        innerRing.layer.borderColor = UIColor.black.cgColor
         innerRing.backgroundColor = innerRingColor
         addSubview(innerRing)
         
         LM = CLLocationManager()
         LM.delegate = self
-        LM.startUpdatingHeading() // Starts updating heading/direction, calls locationManager when changes
+        LM.startUpdatingHeading() // Starts updating heading/direction, calls locationManager on change
     }
-    // Called on change in heading(direction)
-    public func locationManager(manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+    public func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
         if heading == .trueNorth {
-//         print(newHeading.trueHeading) // Use to show direction (degrees) in the output
             northTick.center.x = frame.width/2 // Centers x
-            northTick.center.x += (CGFloat(cosd(-(newHeading.trueHeading + 90))) * (frame.width/2 - min(northTick.frame.width/2, northTick.frame.height/2) - layer.borderWidth))
+            northTick.center.x += (CGFloat(cosd(degrees: -(newHeading.trueHeading + 90))) * (frame.width/2 - min(northTick.frame.width/2, northTick.frame.height/2) - layer.borderWidth))
             
             northTick.center.y = frame.height/2 // Centers y
-            northTick.center.y += (CGFloat(sind(-(newHeading.trueHeading + 90))) * (frame.height/2 - min(northTick.frame.width/2, northTick.frame.height/2) - layer.borderWidth))
+            northTick.center.y += (CGFloat(sind(degrees: -(newHeading.trueHeading + 90))) * (frame.height/2 - min(northTick.frame.width/2, northTick.frame.height/2) - layer.borderWidth))
         }
         if heading == .magneticNorth {
-//         print(newHeading.magneticHeading) // Use to show direction (degrees) in the output
             northTick.center.x = frame.width/2 // Centers x
-            northTick.center.x += (CGFloat(cosd(-(newHeading.magneticHeading + 90))) * (frame.width/2 - min(northTick.frame.width/2, northTick.frame.height/2) - layer.borderWidth))
+            northTick.center.x += (CGFloat(cosd(degrees: -(newHeading.magneticHeading + 90))) * (frame.width/2 - min(northTick.frame.width/2, northTick.frame.height/2) - layer.borderWidth))
             
             northTick.center.y = frame.height/2 // Centers y
-            northTick.center.y += (CGFloat(sind(-(newHeading.magneticHeading + 90))) * (frame.height/2 - min(northTick.frame.width/2, northTick.frame.height/2) - layer.borderWidth))
+            northTick.center.y += (CGFloat(sind(degrees: -(newHeading.magneticHeading + 90))) * (frame.height/2 - min(northTick.frame.width/2, northTick.frame.height/2) - layer.borderWidth))
         }
     }
 }
